95093b48fcce8babb323557f200882973e3d3237
Bethany Woodruff <bethany.w1210@gmail.com>
2019-02-13 16:20:34 -0700

    Organizing files

---
+++ Assignments/Assets/Scripts/Assignments/1/CalicoCat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalicoCat : Feline {
+
+    public bool banded;
+
+    // Use this for initialization
+    void Start()
+    {
+        print("Mew!");
+
+    }
+
+
+}
