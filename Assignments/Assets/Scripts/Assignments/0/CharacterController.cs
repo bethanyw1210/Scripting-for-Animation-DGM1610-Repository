9fc15a288e9a7b5fa000281fa60492ff65f8cdac
Bethany Woodruff <bethany.w1210@gmail.com>
2019-02-20 17:10:29 -0700

    Added coin and coin pickup system

--- Assignments/Assets/Scripts/Assignments/0/CharacterController.cs
+++ Assignments/Assets/Scripts/Assignments/0/CharacterController.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour {
 
     public float moveSpeed=1;
+    public int jumpHeight = 10;
+
+    //player grounded variable 
+    private bool grounded; 
     public GameObject player;
+
 	// Use this for initialization
 	void Start () {
+        // defualt to true when grounded 
+        grounded = true;
 		
 	}
+        // && = this AND that
+        // || = this OR that
+        // !Not this 
 	
 	// Update is called once per frame
 	void Update () {
+        //moves player left and right
         //creating a conditional, when we want to make a decision
-        if(Input.GetKey(KeyCode.D)){
-            player.GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,player.GetComponent<Rigidbody2D>().velocity.y);
+        if(Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,player.GetComponent<Rigidbody2D>().velocity.y);
         }
-        else if(Input.GetKey(KeyCode.A)){
-            player.GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,player.GetComponent<Rigidbody2D>().velocity.y);
+        else if(Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,player.GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        //makes player jump
+        else if(Input.GetKey(KeyCode.Space) && grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight); 
         }
 	}
 
